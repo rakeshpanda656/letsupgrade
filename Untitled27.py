40af9376316df93d1670e8317f35d0b13c957428
rakeshpanda656 <115250976+rakeshpanda656@users.noreply.github.com>
2022-10-07 19:23:46 +0530

    assigment 1

---
+++ Untitled27.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def odd_even(num):
+    if num%2==0:
+        print("even")
+    else:
+        print("odd")
+odd_even(154)
+
+
+# In[ ]:
+
+
+
+
